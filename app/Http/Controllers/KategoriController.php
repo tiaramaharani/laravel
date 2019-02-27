c5267484e4873a73f245164c7cd245cbfd717e2b
tiaramaharani <tiaramaharani13.tm@gmail.com>
2019-02-27 16:15:53 +0700

    tokap24

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -25,6 +25,15 @@ class KategoriController extends Controller
         \Validator::make($req->all(), [
                 'kategori'=>'required|between:3,100|unique:kategori,nama_kategori',
             ])->validate();
-    	return 'Fungsi Save';
+
+    	$result = new Kategori;
+        $result->nama_kategori = $req->kategori;
+
+        if( $result->save() ){
+            return redirect()->route('admin.kategori')
+                    ->with('result','success');
+        } else{
+            return back()->with('result','fail')->withInput();
+        }
     }
 }
--- app/Kategori.php
+++ app/Kategori.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Kategori extends Model
 {
     protected $table = 'kategori';
-    public $timestamp = false;
+    public $timestamps = false;
 }
